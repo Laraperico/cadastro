6e108759daa02ffbe28612dc1b254aa597dd12a2
laraperico <laracrhoft27@gmail.com>
2022-12-14 22:34:58 -0300

    commit actualization

---
+++ src/main/java/br/com/cadastro/cadastro/repositor/PedidoApplication.java
@@ -0,0 +1,9 @@
+package br.com.cadastro.cadastro.repositor;
+
+import org.springframework.boot.SpringApplication;
+
+public class PedidoApplication {
+    public static void main (String[] args ) {
+        SpringApplication.run(PedidoApplication.class, args);}
+    }
+
